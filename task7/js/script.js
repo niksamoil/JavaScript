6b647ae926f94f6f963c92d7686286ea5cb38946
Nikolay <samoil.nikolai@gmail.com>
2020-04-13 15:16:09 +0300

    task 10

--- task7/js/script.js
+++ task7/js/script.js
@@ -34,4 +34,57 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         }
     });
+
+    let deadline = '2020-04-14';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' :  t,
+            'seconds' : seconds,
+            'minutes' : minutes,
+            'hours' : hours
+        };    
+    }
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+
+            if( t.hours < 10 ) {
+                hours.textContent = '0' + t.hours;
+            }
+
+            if( t.minutes < 10 ) {
+                minutes.textContent = '0' + t.minutes;
+            }
+            
+            if ( t.seconds < 10 ) {
+                seconds.textContent = '0' + t.seconds;
+            }
+        }    
+    }
+
+    setClock('timer', deadline);
 });
